757081a47e9ba65955e2b344ee5ed0d096f27f19
Rajveer Singh Gour <rajveersinghgouriitp@gmail.com>
2025-09-18 01:30:26 +0530

    do

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,14 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+
 const app = express();
-const cors = require("cors");
-const ConnectDb = require("./utils/Connectdb");
-const InterviewRouter = require("./routes/Interview.route");
-const EventRouter = require("./routes/Event.route");
-const Reportrouter = require("./routes/reports.route");
-const AdminRouter = require("./routes/admin.route");
-const dotenv = require("dotenv");
-dotenv.config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://focus-hire.vercel.app", "http://localhost:3000"], // allow frontend
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
-ConnectDb();
-
-app.get("/", (req, res) => res.send("Hi dude"));
-
-app.use("/api", InterviewRouter);
-app.use("/events",EventRouter);
-app.use("/reports",Reportrouter);
-app.use("/admin", AdminRouter);
-
-app.listen(3000, () => console.log("Server running on port 3000"));
